986ac17ff13b728cdc6f5c3e668dae571154c39f
Elsa Malle <elsamalle@gmail.com>
2026-04-02 20:04:49 -0300

    Se agrega forma de generar el token

--- generaToken.js
+++ generaToken.js
@@ -1,7 +1,7 @@
 import { generaToken } from './src/utils/generateToken.util.js';
 import {comparePassword, hashPassword} from './src/utils/password.util.js'
 
-hashPassword('miContraseña321');
+//hashPassword('miContraseña321');
 
 //comparePassword('miContraseña123', '$2b$10$H3rHl8.NzOB0NDs3RWIo7OZlI4J93abNzpN.9yAO.h5eAyAnBCI2e');
 
